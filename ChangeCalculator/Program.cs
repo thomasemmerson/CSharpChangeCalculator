1c8cd5e91b4692f12125993c8f996433b758696c
Tom Emmerson <thomasemmerson@users.noreply.github.com>
2016-11-20 16:48:46 +0000

    Initial commit - both code and unit test project

---
+++ ChangeCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter change amount:");
+            double changeAmount = Convert.ToDouble(Console.ReadLine());
+            double numberOfCoins = CalculateNumberOfCoinsIn(changeAmount);
+            Console.WriteLine("Your change contained " + numberOfCoins + "coins!");
+        }
+
+        public static int CalculateNumberOfCoinsIn(double changeAmount)
+        {
+            if (changeAmount == 0.01)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ ChangeCalculatorUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChangeCalculatorUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //TODO Finish first unit test by referencing ChangeCalculator...
+            AssertThat.IsEqual(1, CalculateNumberOfCoinsIn(0.01));
+        }
+    }
+}
